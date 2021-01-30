ed15f0b74494d27b1c63cc10c7be41d0da9f09f6
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-01-30 18:19:46 +0700

    vaccine model with fts(name, shortdes)

---
+++ models/vaccine.model.js
@@ -0,0 +1,53 @@
+const db = require("../utils/database");
+const TABLE_NAME = "vaccine";
+
+module.exports = {
+  //---------------------default query----------------------------
+  getAll() {
+    return db.load(`select * from ${TABLE_NAME}`);
+  },
+
+  add(newObj) {
+    return db.add(newObj, TABLE_NAME);
+  },
+
+  del(Obj) {
+    const condition = { id: Obj.id };
+    return db.del(condition, TABLE_NAME);
+  },
+
+  update(Obj) {
+    const condition = { id: Obj.id };
+    return db.update(Obj, condition, TABLE_NAME);
+  },
+
+  //---------------------others select----------------------------
+  //get an single user by user id
+  async getSingle(id) {
+    const rows = await db.load(`select * from ${TABLE_NAME} where id = ${id} `);
+    if (rows.length === 0) return null;
+    return rows[0];
+  },
+
+  async getSingleByName(name) {
+    const rows = await db.load(
+      `select * from ${TABLE_NAME} where name = "${name}"`
+    );
+    if (rows.length === 0) return null;
+    return rows[0];
+  },
+
+  //fulltext search with querystring
+  searchAll(querystring) {
+    return db.load(
+      `SELECT * FROM ${TABLE_NAME} WHERE MATCH (shortdes, name) AGAINST ('${querystring}' );`
+    );
+  },
+
+  //---------------------others update----------------------------
+
+  //function to update avatar link with id and link
+  changeLink(id, link) {
+    return db.load(`update ${TABLE_NAME} set link='${link}' where id=${id}`);
+  },
+};
--- routes/test.route.js
+++ routes/test.route.js
@@ -3,11 +3,12 @@ const diaryModel = require("../models/diary.model");
 const lullabyModel = require("../models/lullaby.model");
 const newsModel = require("../models/news.model");
 const recipeModel = require("../models/recipe.model");
+const vaccineModel = require("../models/vaccine.model");
 const router = express.Router();
 
 router.get("/", async function (req, res) {
   const query = req.query.a;
-  const ret = await recipeModel.searchAll(query);
+  const ret = await vaccineModel.searchAll(query);
   res.send(ret);
 });
 
