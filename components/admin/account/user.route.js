d88a97b9f7bc48cbc9080e5442bd570e20f47ba2
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-11 00:15:12 +0700

    admin route for user management

---
+++ components/admin/account/user.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const userController = require("./user.controller");
+const router = express.Router();
+
+router.get("/", userController.getAllUser);
+router.get("/basis-user", userController.getBasisUser);
+router.get("/editor", userController.getEditor);
+
+router.post("/diable-user", userController.disableUser);
+
+router.post("/register", userController.register);
+
+module.exports = router;
