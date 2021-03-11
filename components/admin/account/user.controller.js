452138ae6b66aedb166fe8a8a7087606107081db
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-11 16:48:13 +0700

    [admin] register editor acc module

--- components/admin/account/user.controller.js
+++ components/admin/account/user.controller.js
@@ -41,10 +41,72 @@ module.exports = accountController = {
   },
 
   registerUser: async function (req, res) {
-    try {
-      res.send({ success: true });
-    } catch (error) {
-      res.send({ success: false, err_message: error });
+    const checkEmail = await userModel.getSingleByEmail(req.body.mail);
+    const checkUsername = await userModel.getSingleByUsername(
+      req.body.username
+    );
+
+    if (checkEmail === null) {
+      if (checkUsername === null) {
+        try {
+          const hashedPass = bcrypt.hashSync(
+            req.body.password,
+            parseInt(process.env.ACCOUNT_TIMEHASHPASS)
+          );
+          const convertedDOB = moment(req.body.dob, "DD/MM/YYYY").format(
+            "YYYY-MM-DD"
+          );
+
+          const newUser = {
+            username: req.body.username,
+            password: hashedPass,
+            dob: convertedDOB,
+            fullname: req.body.fullname,
+            email: req.body.mail,
+            permission: 2, //editor user
+            isDisable: 0,
+            isVerified: 0,
+          };
+          console.log(newUser);
+
+          //add user data to db
+          await userModel.add(newUser);
+
+          //send email confirm
+          var transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: process.env.CONTACT_EMAIL,
+              pass: process.env.EMAIL_PASSWORD,
+            },
+          });
+
+          let otp = Math.random().toString(36).substring(7);
+
+          var mailOptions = {
+            from: process.env.CONTACT_EMAIL,
+            to: req.body.mail,
+            subject: "Children Health Monitoring confirm account",
+            text: "Welcome to our editor's community. Here is your otp: " + otp,
+          };
+
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          });
+
+          res.json({ success: true });
+        } catch (error) {
+          res.json({ success: false, err_message: error });
+        }
+      } else {
+        res.json({ success: false, err_message: "existed username" });
+      }
+    } else {
+      res.json({ success: false, err_message: "existed email" });
     }
   },
 };
--- components/admin/account/user.route.js
+++ components/admin/account/user.route.js
@@ -6,7 +6,7 @@ router.get("/", userController.getAllUser);
 router.get("/basis-user", userController.getBasisUser);
 router.get("/editor", userController.getEditor);
 
-router.post("/diable-user", userController.disableUser);
+router.post("/disable-user", userController.disableUser);
 
 router.post("/register", userController.registerUser);
 
