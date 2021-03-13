a81f691d443df3350e79819dc9783944d38aa8ef
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-13 19:13:26 +0700

    Update diary-vaccine.model.js
    add method to del event "logically"

--- models/diary-vaccine.model.js
+++ models/diary-vaccine.model.js
@@ -4,7 +4,7 @@ const TABLE_NAME = "diary_vaccine";
 module.exports = {
   //---------------------default query----------------------------
   getAll() {
-    return db.load(`select * from ${TABLE_NAME}`);
+    return db.load(`select * from ${TABLE_NAME} and isDel=false`);
   },
 
   add(newObj) {
@@ -24,23 +24,39 @@ module.exports = {
   //---------------------others select----------------------------
   //get an single user by user id
   async getSingle(id) {
-    const rows = await db.load(`select * from ${TABLE_NAME} where id = ${id} `);
+    const rows = await db.load(
+      `select * from ${TABLE_NAME} where id = ${id} and isDel=false`
+    );
     if (rows.length === 0) return null;
     return rows[0];
   },
 
-  getAllByVaccineId(id) {
-    return db.load(`select * from ${TABLE_NAME} where id_vaccine=${id}`);
+  //get diary id by event's id to authentication
+  async getIdDiary(id) {
+    const rows = await db.load(
+      `select id_diary from ${TABLE_NAME} where id = ${id} and isDel=false`
+    );
+    if (rows.length === 0) return null;
+    return rows[0].id_diary;
   },
 
   getAllByDiaryId(id) {
-    return db.load(`select * from ${TABLE_NAME} where id_diary=${id}`);
+    return db.load(
+      `select * from ${TABLE_NAME} where id_diary=${id} and isDel=false`
+    );
   },
 
   //---------------------others update----------------------------
 
   //function to update avatar link with id and link
   setAvatar(id, link) {
-    return db.load(`update ${TABLE_NAME} set image='${link}' where id=${id}`);
+    return db.load(
+      `update ${TABLE_NAME} set image='${link}' where id=${id} and isDel=false`
+    );
+  },
+
+  //function delete event "logically"
+  setDelete(id) {
+    return db.load(`update ${TABLE_NAME} set isDel=true where id=${id}`);
   },
 };
