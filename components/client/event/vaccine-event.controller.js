0cc6a94a1d1a58d17dbdb6ec44d0c4cf2e28f8ae
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-13 19:44:49 +0700

    new vaccine's event module

--- components/client/event/vaccine-event.controller.js
+++ components/client/event/vaccine-event.controller.js
@@ -19,3 +19,39 @@ module.exports = diaryController = {
     }
   },
 
+  newEvent: async function (req, res) {
+    try {
+      const fileUploaded = req.files.uploadImg;
+      //upload file to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(
+        fileUploaded.tempFilePath,
+        {
+          upload_preset: process.env.CLOUD_DIARY_VACCINE_TRACK_PRESET, //choose configed preset to store image
+        }
+      );
+
+      //create new event according to user input
+      const newEvent = {
+        id_diary: req.query.id, //id of log in account
+        log_date: moment(req.body.log_date, "DD/MM/YYYY").format("YYYY-MM-DD"),
+        note: req.body.note,
+        image: uploadResponse.url || "",
+        vaccine: req.body.vaccine,
+        vaccineName: req.body.vaccineName,
+        doctor: req.body.doctor,
+        isDel: 0,
+      };
+
+      //add new diary to db
+      const ret = await diaryVaccineModel.add(newEvent);
+
+      //get full datum back from db to check add successfully
+      const datum = await diaryVaccineModel.getSingle(ret.insertId);
+
+      //send success message to client
+      res.send({ success: true, eventInfor: datum });
+    } catch (error) {
+      res.send({ success: false, err_message: error });
+    }
+  },
+
