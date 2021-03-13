937efd4e0d3ebbf0c8a3fed968022d38f38ad85a
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-14 00:29:01 +0700

    Update errors.mdw.js

--- middlewares/errors.mdw.js
+++ middlewares/errors.mdw.js
@@ -1,16 +1,12 @@
 module.exports = function (app) {
   //default route
   app.use(function (req, res) {
-    res.render("404", {
-      layout: false,
-    });
+    res.send("404");
   });
 
   // false url err
   app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.render("500", {
-      layout: false,
-    });
+    res.send("500");
   });
 };
