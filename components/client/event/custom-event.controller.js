57b01d568a5de69ac483fbc4daf661e868820e54
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-18 16:45:23 +0700

    [custom-event]get all event

---
+++ components/client/event/custom-event.controller.js
@@ -0,0 +1,22 @@
+const diaryCustomModel = require("../../../models/diairy-custom.model");
+const moment = require("moment");
+const cloudinary = require("../../../middlewares/cloudinary.mdw");
+const utilFuncs = require("../../../utils/util-function");
+const { updateLocale } = require("moment");
+
+module.exports = diaryController = {
+  getAllEvent: async function (req, res) {
+    try {
+      const data = await diaryCustomModel.getAllByDiaryId(req.query.id);
+
+      //sort data by date descending
+      data.sort(utilFuncs.compareDesc);
+
+      //send data to client
+      res.send({ success: true, events: data });
+    } catch (error) {
+      res.status(406).send({ success: false, err_message: error });
+    }
+  },
+
+};
