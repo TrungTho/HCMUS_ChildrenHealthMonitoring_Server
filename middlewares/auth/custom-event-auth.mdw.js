dc8863506fc96039a9fcfbef2b6482a1e30ad8b4
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-18 16:38:19 +0700

    Create custom-event-auth.mdw.js

---
+++ middlewares/auth/custom-event-auth.mdw.js
@@ -0,0 +1,24 @@
+const customDiaryModel = require("../../models/diairy-custom.model");
+
+//middleware function to check right of client to access event
+//user req has id_diary in req.param.id & id of event in req.body.id
+//so we will check those id is belong to each other or not
+module.exports = async function DiaryAuth(req, res, next) {
+  try {
+    const eventIdDiary = await customDiaryModel.getIdDiary(req.body.id);
+    //compare user_id of this diary with log in user
+    if (parseInt(req.query.id) !== eventIdDiary) {
+      return res.status(403).send({
+        success: false,
+        err_message: "invalid request4",
+      });
+    }
+  } catch (error) {
+    return res
+      .status(403)
+      .send({ success: false, err_message: "invalid request" });
+  }
+
+  //allow to access if this event belong to this diary
+  next();
+};
