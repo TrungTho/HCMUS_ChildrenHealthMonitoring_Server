bcfb889e5f43841b9cb372563514baee4794ed93
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-18 19:58:52 +0700

    [tip-model] update column isApproved

--- models/tip.model.js
+++ models/tip.model.js
@@ -4,7 +4,7 @@ const TABLE_NAME = "tip";
 module.exports = {
   //---------------------default query----------------------------
   getAll() {
-    return db.load(`select * from ${TABLE_NAME}`);
+    return db.load(`select * from ${TABLE_NAME} where isApproved=true`);
   },
 
   add(newObj) {
@@ -24,9 +24,16 @@ module.exports = {
   //---------------------others select----------------------------
   //get an single user by user id
   async getSingle(id) {
-    const rows = await db.load(`select * from ${TABLE_NAME} where id = ${id} `);
+    const rows = await db.load(
+      `select * from ${TABLE_NAME} where id = ${id} where isApproved=true`
+    );
     if (rows.length === 0) return null;
     return rows[0];
+  },
+
+  //function to get all tus with any approval state
+  adminGetAll() {
+    return db.load(`select * from ${TABLE_NAME}`);
   },
 
   //fulltext search with querystring
@@ -41,7 +48,14 @@ module.exports = {
   //function to update main cover link with id and link
   changeMainCover(id, link) {
     return db.load(
-      `update ${TABLE_NAME} set main_cover='${link}' where id=${id}`
+      `update ${TABLE_NAME} set main_cover='${link}' where id=${id} and isApproved=true`
+    );
+  },
+
+  //function to flip state approval of tus
+  flipApproval(id) {
+    return db.load(
+      `update ${TABLE_NAME} set isApproved=not(isApproved) where id=${id}`
     );
   },
 };
