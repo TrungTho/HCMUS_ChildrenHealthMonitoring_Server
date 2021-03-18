9fc5e90defa5bcca0fde5df49b895cc92ca08b68
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-18 16:37:34 +0700

    Create diairy-custom.model.js

---
+++ models/diairy-custom.model.js
@@ -0,0 +1,62 @@
+const db = require("../utils/database");
+const TABLE_NAME = "diary_custom";
+
+module.exports = {
+  //---------------------default query----------------------------
+  getAll() {
+    return db.load(`select * from ${TABLE_NAME}  and isDel=false`);
+  },
+
+  add(newObj) {
+    return db.add(newObj, TABLE_NAME);
+  },
+
+  del(Obj) {
+    const condition = { id: Obj.id };
+    return db.del(condition, TABLE_NAME);
+  },
+
+  update(Obj) {
+    const condition = { id: Obj.id };
+    return db.update(Obj, condition, TABLE_NAME);
+  },
+
+  //---------------------others select----------------------------
+  //get an single user by user id
+  async getSingle(id) {
+    const rows = await db.load(
+      `select * from ${TABLE_NAME} where id = ${id}  and isDel=false`
+    );
+    if (rows.length === 0) return null;
+    return rows[0];
+  },
+
+  //get diary id by event's id to authentication
+  async getIdDiary(id) {
+    const rows = await db.load(
+      `select id_diary from ${TABLE_NAME} where id = ${id} and isDel=false`
+    );
+    if (rows.length === 0) return null;
+    return rows[0].id_diary;
+  },
+
+  getAllByDiaryId(id) {
+    return db.load(
+      `select * from ${TABLE_NAME} where id_diary=${id} and isDel=false`
+    );
+  },
+
+  //---------------------others update----------------------------
+
+  //function to update avatar link with id and link
+  changeImage(id, link) {
+    return db.load(
+      `update ${TABLE_NAME} set image='${link}' where id=${id} and isDel=false`
+    );
+  },
+
+  //function delete event "logically"
+  setDelete(id) {
+    return db.load(`update ${TABLE_NAME} set isDel=true where id=${id}`);
+  },
+};
