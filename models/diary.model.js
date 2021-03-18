dcecdffc6952a565821bf8dfff4431dcf4fd4f24
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-18 19:41:13 +0700

    Update diary.model.js
    add condition to check isdel value

--- models/diary.model.js
+++ models/diary.model.js
@@ -4,7 +4,7 @@ const TABLE_NAME = "diary";
 module.exports = {
   //---------------------default query----------------------------
   getAll() {
-    return db.load(`select * from ${TABLE_NAME}`);
+    return db.load(`select * from ${TABLE_NAME} where isDel=false`);
   },
 
   add(newObj) {
@@ -24,19 +24,25 @@ module.exports = {
   //---------------------others select----------------------------
   //get an single user by user id
   async getSingle(id) {
-    const rows = await db.load(`select * from ${TABLE_NAME} where id = ${id} `);
+    const rows = await db.load(
+      `select * from ${TABLE_NAME} where id = ${id} and isDel=false`
+    );
     if (rows.length === 0) return null;
     return rows[0];
   },
 
   getAllByUserId(id_user) {
-    return db.load(`select * from ${TABLE_NAME} where id_user=${id_user}`);
+    return db.load(
+      `select * from ${TABLE_NAME} where id_user=${id_user} and isDel=false`
+    );
   },
 
   //---------------------others update----------------------------
 
   //function to update avatar link with id and link
   setAvatar(id, link) {
-    return db.load(`update ${TABLE_NAME} set avatar='${link}' where id=${id}`);
+    return db.load(
+      `update ${TABLE_NAME} set avatar='${link}' where id=${id} and isDel=false`
+    );
   },
 };
