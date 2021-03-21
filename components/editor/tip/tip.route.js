cc19af6da071bbf10b0e78dbd4b41122c8bf655d
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-22 00:14:39 +0700

    Create tip.route.js

---
+++ components/editor/tip/tip.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const tipController = require("./tip.controller");
+const editorPostAuth = require("../../../middlewares/auth/editor-post-auth.mdw");
+const router = express.Router();
+
+router.get("/my-post", tipController.getAllPost);
+
+router.post("/new-post", tipController.newPost);
+router.post("/update-post", editorPostAuth, tipController.updatePost);
+router.post("/delete-post", editorPostAuth, tipController.deletePost);
+
+module.exports = router;
