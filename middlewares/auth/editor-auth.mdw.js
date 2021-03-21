25e4c8e31148e14516148354f284aa7c80ebf8aa
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-21 16:42:01 +0700

    Create editor-auth.mdw.js

---
+++ middlewares/auth/editor-auth.mdw.js
@@ -0,0 +1,12 @@
+//middleware function to check right of client to access profile
+
+module.exports = function editorAuth(req, res, next) {
+  if (req.user.permission === "2") {
+  } else {
+    return res
+      .status(403)
+      .send({ success: false, err_message: "invalid request" });
+  }
+
+  next();
+};
