52ea00f4e7eb2e59a8f266d5c802406daf51b11e
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-03-26 17:34:12 +0700

    [handbook-tip] get all tips module

---
+++ components/client/handbook/tip.controller.js
@@ -0,0 +1,25 @@
+const tipModel = require("../../../models/tip.model");
+const moment = require("moment");
+const utilFuncs = require("../../../utils/util-function");
+const { updateLocale } = require("moment");
+
+module.exports = diaryController = {
+  getAllPost: async function (req, res) {
+    try {
+      const data = await tipModel.getAll();
+
+      //send data to client
+      res.send({ success: true, posts: data });
+    } catch (error) {
+      res.status(406).send({ success: false, err_message: error });
+    }
+  },
+
+  template: async function (req, res) {
+    try {
+      res.send({ success: true });
+    } catch (error) {
+      res.status(406).send({ success: false, err_message: error });
+    }
+  },
+};
