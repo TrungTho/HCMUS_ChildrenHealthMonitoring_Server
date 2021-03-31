d3d8b47cf08de2428fd61c22fac6387eb85a0367
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-04-01 00:58:49 +0700

    [admin-notification] send new noti controller

--- components/admin/notification/notification.controller.js
+++ components/admin/notification/notification.controller.js
@@ -12,7 +12,24 @@ module.exports = userController = {
 
   sendNotification: async function (req, res) {
     try {
-      res.send({ success: true });
+      //build data from user input
+      const newNoti = {
+        id_from: req.user.id,
+        id_to: req.body.id_dest,
+        detail: req.body.detail,
+        sendDate: new Date(),
+        isRead: 0,
+        isDel: 0,
+        username_from: req.user.username,
+      };
+
+      //add data to db
+      const ret = await notificationModel.add(newNoti);
+
+      //get datum just added in db to client
+      const datum = await notificationModel.getSingle(ret.insertId);
+
+      res.send({ success: true, notification: datum });
     } catch (error) {
       res.status(406).send({ success: false, err_message: error });
     }
