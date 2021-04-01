1abdfa239c8ed83335683560fc6404925df8f43f
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-04-01 20:32:53 +0700

    [admin-post] get all post depend on timeline (sorted)

--- components/admin/post/post.controller.js
+++ components/admin/post/post.controller.js
@@ -1,3 +1,8 @@
+const newsModel = require("../../../models/news.model");
+const recipeModel = require("../../../models/recipe.model");
+const tipModel = require("../../../models/tip.model");
+const utilFunction = require("../../../utils/util-function");
+
 module.exports = postController = {
   approvePost: async function (req, res) {
     try {
@@ -9,7 +14,19 @@ module.exports = postController = {
 
   getAllPost: async function (req, res) {
     try {
-      res.send({ success: true });
+      //get data from db
+      const tipPosts = await tipModel.adminGetAll();
+      const recipePosts = await recipeModel.adminGetAll();
+      const vaccinePosts = await newsModel.adminGetAll();
+
+      //combine 3 type of post to one
+      const allPosts = tipPosts.concat(recipePosts).concat(vaccinePosts);
+
+      //sort posts depend on createDate
+      allPosts.sort(utilFunction.compareDesc);
+
+      //send data to client
+      res.send({ success: true, posts: allPosts });
     } catch (error) {
       res.status(406).send({ success: false, err_message: error });
     }
