f9c9e2592a72cffca81cf07792de43360af36697
TrungTho <41388666+TrungTho@users.noreply.github.com>
2021-06-20 18:20:59 +0700

    [handbook-tip]: add paging param

--- components/client/handbook/tip.controller.js
+++ components/client/handbook/tip.controller.js
@@ -2,15 +2,31 @@ const tipModel = require("../../../models/tip.model");
 const moment = require("moment");
 const utilFuncs = require("../../../utils/util-function");
 const { updateLocale } = require("moment");
+const itemPerPage = 5;
 
 module.exports = tipController = {
   getAllPost: async function (req, res) {
     try {
-      const data = await tipModel.getAll();
-
+      let data, totalPages;
+      if (req.query.pageNo) {
+        data = await tipModel.getAllWithPaging(
+          parseInt(req.query.pageNo),
+          itemPerPage
+        );
+        // console.log("data: " + data);
+        const rowNo = parseInt(await tipModel.countRows());
+        // console.log("row count: ", rowNo);
+        totalPages =
+          Math.floor(rowNo / itemPerPage) + (rowNo % itemPerPage === 0 ? 0 : 1);
+        // console.log("pages: " + totalPages);
+      } else {
+        data = await tipModel.getAll();
+        totalPages = 0;
+      }
       //send data to client
-      res.send({ success: true, posts: data });
+      res.send({ success: true, totalPages, posts: data });
     } catch (error) {
+      console.log(error);
       res.status(406).send({ success: false, err_message: error });
     }
   },
